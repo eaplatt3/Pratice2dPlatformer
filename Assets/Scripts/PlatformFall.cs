3ed1cd86fbba2d4416fc11874ff737c29f2a5165
Earl Platt III <36335481+eaplatt3@users.noreply.github.com>
2019-04-14 17:33:58 -0400

    Added Falling Platforms

---
+++ Assets/Scripts/PlatformFall.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformFall : MonoBehaviour
+{
+
+    public float fallDelay = 1f;
+
+    private Rigidbody2D rb2d;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+     void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+            Invoke("Fall", fallDelay);
+
+    }
+
+    void Fall()
+    {
+        rb2d.isKinematic = false;
+    }
+
+}
