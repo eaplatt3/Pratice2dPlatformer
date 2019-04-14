b469151171af05556c9f69773b9970fb7c1c1327
Earl Platt III <36335481+eaplatt3@users.noreply.github.com>
2019-04-14 17:04:44 -0400

    Finished Spawn Monitor

--- Assets/Scripts/SpawnMonitor.cs
+++ Assets/Scripts/SpawnMonitor.cs
@@ -2,17 +2,35 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class SpawnMonitor : MonoBehaviour
 {
+
+    public int maxPlatforms = 20;
+    public GameObject platform;
+
+    //Max & Min Spawning Distance from each Platform 
+    public float horizontalMin = 7.5f;
+    public float horizontalMax = 14f;
+    public float verticalMin = -6f;
+    public float verticalMax = 6f;
+
+    private Vector2 originPosition;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        originPosition = transform.position; //1st Postion will be Transform of Spawn Manager
+        Spawn();
     }
 
-    // Update is called once per frame
-    void Update()
+   void Spawn()
     {
-        
+        //Spawns Plaforms Randomly 
+        for(int i = 0; i < maxPlatforms; i++)
+        {
+            Vector2 randomPosition = originPosition + new Vector2(Random.Range(horizontalMin, horizontalMax), Random.Range(verticalMin, verticalMax));
+            Instantiate(platform, randomPosition, Quaternion.identity); //Quaternion -> No Rotation
+            originPosition = randomPosition; //New Origin is last Random Position
+        }
     }
 }
